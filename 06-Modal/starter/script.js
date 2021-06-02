6bcf6eb42a464b6ef24208f3f2a27b740192c80d
Amar Nath <nthvpsn@gmail.com>
2021-06-02 16:56:08 +0530

    Update Modal Folder

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('.show-modal');
+
+const close=function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+const open =function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+// console.log(showModal);
+for (let i = 0; i < showModal.length; i++)
+    console.log(showModal[i].addEventListener('click', open));
+
+closeModal.addEventListener('click', close);
+overlay.addEventListener('click', close);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden'))
+        close();
+})
